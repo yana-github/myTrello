25e4b285f356cf2dafcbfc49402eaca62e88d757
Yana <yana.0.yakimchuk@gmail.com>
2022-09-08 16:05:46 +0300

    chahge styles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const root = document.querySelector("#root");
 
-export function createEl(tag, className, text, type, placeholder) {
+/*  function createEl(tag, className, text, type, placeholder) {
   let el = document.createElement(tag);
   className ? el.classList.add(className) : null;
   text ? (el.innerText = text) : null;
@@ -11,6 +11,20 @@ export function createEl(tag, className, text, type, placeholder) {
   return el;
 }
 
+ */
+
+function createEl(tag, className, text) {
+  let el = document.createElement(tag);
+  text ? (el.innerText = text) : null;
+
+  if (className) {
+      let arr = className.split(' ');
+      for (let elArr of arr) {
+          el.classList.add(elArr);
+      }
+  }
+  return el;
+}
 
 
 //добавить основные элементы
@@ -21,28 +35,27 @@ let clock = createEl("div", "header__clock");
 clock.id = "minSec";
 
 let board = createEl("section", "board");
-let boardRow = createEl("div", "board__row");
+let boardRow = createEl("div", "board__row row");
 
-let boardTodo = createEl("div", "board__todo");
-
-let boardTodoHeader = createEl("div", "board-header");
+let boardTodo = createEl("div", "board__item col-4 col-md-6 col-sm-12");
+let boardTodoHeader = createEl("div", "board-header todo-header");
 let boardTodoH2 = createEl("h2", "board-header__title", "Todo");
 let boardTodoCount = createEl("span", "board-header__count", "0");
 let boardTodoList = createEl("ul", "board-list");
-let addBtn = createEl("button", "board__todo-btn", "Add todo");
+let addBtn = createEl("button", "board-btn add-btn", "Add todo");
 
-let boardProgress = createEl("div", "board__progress");
-let boardProgressHeader = createEl("div", "board-header");
+let boardProgress = createEl("div", "board__item col-4 col-md-6 col-sm-12");
+let boardProgressHeader = createEl("div", "board-header progress-header");
 let boardProgressH2 = createEl("h2", "board-header__title", "In Progress");
 let boardProgressCount = createEl("span", "board-header__count", "0");
 let boardProgressList = createEl("ul", "board-list");
 
-let boardDone = createEl("div", "board__done");
-let boardDoneHeader = createEl("div", "board-header");
+let boardDone = createEl("div", "board__item col-4 col-md-6 col-sm-12");
+let boardDoneHeader = createEl("div", "board-header done-header");
 let boardDoneH2 = createEl("h2", "board-header__title", "Done");
 let boardDoneCount = createEl("span", "board-header__count", "0");
 let boardDoneList = createEl("ul", "board-list");
-let deleteAllBtn = createEl("button", "board__done-btn", "Delete All");
+let deleteAllBtn = createEl("button", "board-btn delete-btn", "Delete All");
 
 
 
